496d84f0a149166b4955dc770dd79fd738818262
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2024-01-29 22:45:31 +0700

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,75 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { Container } from 'reactstrap'
+import logo from '../../assets/images/logo.png'
+import { NavLink, Link } from 'react-router-dom'
+import '../../styles/header.scss'
+
+const nav__link = [
+  {
+    display: 'Home',
+    path: '/home'
+  },
+  {
+    display: 'Foods',
+    path: '/foods',
+  },
+  {
+    display: 'Cart',
+    path: '/cart',
+  },
+  {
+    display: 'Contact',
+    path: '/contact',
+  }
+]
 
 const Header = () => {
+
+  const menuRef = useRef(null)
+  const toggleMenu = () => menuRef.current.classList.toggle('show__menu')
+
   return (
-    <div>
-      Header
-    </div>
+    <header className='header'>
+      <Container>
+        <div className='nav__wrapper d-flex align-items-center justify-content-between'>
+          <div className='logo'>
+            <img src={logo} alt='logo' />
+            <h5>TV Food</h5>
+          </div>
+
+          {/* ====== menu ======== */}
+          <div className='navigation' ref={menuRef} onClick={toggleMenu}>
+            <div className='menu d-flex align-items-center gap-5'>
+              {nav__link.map((item, index) => (
+                <NavLink
+                  to={item.path}
+                  key={index}
+                  className={navClass => navClass.isActive ? 'active__menu' : ''}
+                >
+                  {item.display}
+                </NavLink>
+              ))}
+            </div>
+          </div>
+
+          {/* ======== nav icons ======== */}
+          <div className='nav__right d-flex align-items-center gap-4'>
+            <span className='cart__icon'>
+              <i class='ri-shopping-basket-2-line'></i>
+              <span className='cart__badge'>5</span>
+            </span>
+
+            <span className='user'>
+              <Link to='/login'><i class='ri-user-line'></i></Link>
+            </span>
+
+            <span className='mobile__menu' onClick={toggleMenu}>
+              <i class='ri-menu-line'></i>
+            </span>
+          </div>
+        </div>
+      </Container>
+    </header>
   )
 }
 

50f55d8fa3a6e6180d2f541025ac04535e157593
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2024-02-01 00:13:45 +0700

    update

--- src/components/UI/product-card/ProductCard.jsx
+++ src/components/UI/product-card/ProductCard.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import productImg from '../../../assets/images/product_2.1.jpg'
+import { Link } from 'react-router-dom'
 
 const ProductCard = () => {
   return (
-    <div>
-      ProductCard
+    <div className='product__item'>
+      <div className="product__img">
+        <img src={productImg} alt="product-img" />
+      </div>
+
+      <div className="product__content">
+        <h5><Link to=''>Burger</Link></h5>
+        <div>
+          <span className='product__price'>$24.00</span>
+          <button className='addToCart__btn'>Add To Cart</button>
+        </div>
+      </div>
     </div>
   )
 }

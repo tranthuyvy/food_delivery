95c693aaafebdd147e1819831525a8642398b00b
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2024-02-02 23:08:13 +0700

    section why choose

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,65 +1,66 @@
-import React, { useState, useEffect } from "react";
-import Helmet from "../components/Helmet/Helmet";
-import { Container, Row, Col } from "reactstrap";
-import heroImg from "../assets/images/hero.png";
-import { Link } from "react-router-dom";
-import Category from "../components/UI/category/Category";
-import featureImg01 from "../assets/images/service-01.png";
-import featureImg02 from "../assets/images/service-02.png";
-import featureImg03 from "../assets/images/service-03.png";
-import products from "../assets/fake-data/products";
-import foodCategoryImg01 from "../assets/images/hamburger.png";
-import foodCategoryImg02 from "../assets/images/pizza.png";
-import foodCategoryImg03 from "../assets/images/bread.png";
-import ProductCard from "../components/UI/product-card/ProductCard";
-import "../styles/home.scss";
+import React, { useState, useEffect } from 'react';
+import Helmet from '../components/Helmet/Helmet';
+import { Container, Row, Col, ListGroup, ListGroupItem } from 'reactstrap';
+import heroImg from '../assets/images/hero.png';
+import { Link } from 'react-router-dom';
+import Category from '../components/UI/category/Category';
+import featureImg01 from '../assets/images/service-01.png';
+import featureImg02 from '../assets/images/service-02.png';
+import featureImg03 from '../assets/images/service-03.png';
+import products from '../assets/fake-data/products';
+import foodCategoryImg01 from '../assets/images/hamburger.png';
+import foodCategoryImg02 from '../assets/images/pizza.png';
+import foodCategoryImg03 from '../assets/images/bread.png';
+import ProductCard from '../components/UI/product-card/ProductCard';
+import whyImg from '../assets/images/location.png';
+import '../styles/home.scss';
 
 const featureData = [
   {
-    title: "Quick Delivery",
+    title: 'Quick Delivery',
     imgUrl: featureImg01,
-    desc: "Nhanh chóng nhận hàng",
+    desc: 'Nhanh chóng nhận hàng',
   },
   {
-    title: "Super Dine In",
+    title: 'Super Dine In',
     imgUrl: featureImg02,
-    desc: "Thức uống ngon lành",
+    desc: 'Thức uống ngon lành',
   },
   {
-    title: "Easy Pick Up",
+    title: 'Easy Pick Up',
     imgUrl: featureImg03,
-    desc: "Dễ dàng đặt hàng",
+    desc: 'Dễ dàng đặt hàng',
   },
 ];
 
 const Home = () => {
-  const [category, setCategory] = useState("ALL");
+  const [category, setCategory] = useState('ALL');
   const [allProducts, setAllProducts] = useState(products);
 
   useEffect(() => {
-    if (category === "ALL") {
+    if (category === 'ALL') {
       setAllProducts(products);
     }
 
-    if (category === "BURGER") {
+    if (category === 'BURGER') {
       const filteredProducts = products.filter(
-        (item) => item.category === "Burger"
+        (item) => item.category === 'Burger'
       );
 
       setAllProducts(filteredProducts);
     }
 
-    if (category === "PIZZA") {
+    if (category === 'PIZZA') {
       const filteredProducts = products.filter(
-        (item) => item.category === "Pizza"
+        (item) => item.category === 'Pizza'
       );
 
       setAllProducts(filteredProducts);
     }
 
-    if (category === "BREAD") {
+    if (category === 'BREAD') {
       const filteredProducts = products.filter(
-        (item) => item.category === "Bread"
+        (item) => item.category === 'Bread'
       );
 
       setAllProducts(filteredProducts);
@@ -67,40 +68,40 @@ const Home = () => {
   }, [category]);
 
   return (
-    <Helmet title="Home">
+    <Helmet title='Home'>
       <section>
         <Container>
           <Row>
-            <Col lg="6" md="6">
-              <div className="hero__content">
-                <h5 className="mb-3">Easy way to make an order</h5>
-                <h1 className="mb-4 hero__title">
+            <Col lg='6' md='6'>
+              <div className='hero__content'>
+                <h5 className='mb-3'>Easy way to make an order</h5>
+                <h1 className='mb-4 hero__title'>
                   <span>HUNGRY?</span> Just wait <br />
                   food at<span> your door</span>
                 </h1>
                 <p>Day la mot doan cuc ky cuc ky very very dai thong</p>
 
-                <div className="hero__btns d-flex align-items-center gap-5 mt-4">
-                  <button className="order__btn d-flex align-items-center justify-content-between ">
-                    Order Now <i class="ri-arrow-right-circle-fill"></i>
+                <div className='hero__btns d-flex align-items-center gap-5 mt-4'>
+                  <button className='order__btn d-flex align-items-center justify-content-between '>
+                    Order Now <i class='ri-arrow-right-circle-fill'></i>
                   </button>
 
-                  <button className="all__foods-btn">
-                    <Link to="/foods">See all foods</Link>
+                  <button className='all__foods-btn'>
+                    <Link to='/foods'>See all foods</Link>
                   </button>
                 </div>
 
-                <div className="hero__service d-flex align-items-center gap-5 mt-5">
-                  <p className="d-flex align-items-center gap-2">
-                    <span className="shipping__icon">
-                      <i class="ri-car-line"></i>
+                <div className='hero__service d-flex align-items-center gap-5 mt-5'>
+                  <p className='d-flex align-items-center gap-2'>
+                    <span className='shipping__icon'>
+                      <i class='ri-car-line'></i>
                     </span>
                     No shipping charge
                   </p>
 
-                  <p className="d-flex align-items-center gap-2">
-                    <span className="shipping__icon">
-                      <i class="ri-shield-check-line"></i>
+                  <p className='d-flex align-items-center gap-2'>
+                    <span className='shipping__icon'>
+                      <i class='ri-shield-check-line'></i>
                     </span>
                     100% secure checkout
                   </p>
@@ -108,41 +109,41 @@ const Home = () => {
               </div>
             </Col>
 
-            <Col lg="6" md="6">
-              <div className="hero__img">
-                <img src={heroImg} alt="hero-img" className="w-100" />
+            <Col lg='6' md='6'>
+              <div className='hero__img'>
+                <img src={heroImg} alt='hero-img' className='w-100' />
               </div>
             </Col>
           </Row>
         </Container>
       </section>
 
-      <section className="pt-0">
+      <section className='pt-0'>
         <Category />
       </section>
 
-      <section className="section__feature">
+      <section className='section__feature'>
         <Container>
           <Row>
-            <Col lg="12" className="text-center">
-              <h5 className="feature__subtitle mb-4">What we serve</h5>
-              <h2 className="feature__title">Just sit back at home</h2>
-              <h2 className="feature__title">
+            <Col lg='12' className='text-center'>
+              <h5 className='feature__subtitle mb-4'>What we serve</h5>
+              <h2 className='feature__title'>Just sit back at home</h2>
+              <h2 className='feature__title'>
                 we will <span>take care</span>
               </h2>
-              <p className="mb-1 mt-4 feature__text">Huong Thom Tu Dat Viet</p>
-              <p className="feature__text">Website build using ReactJS</p>
+              <p className='mb-1 mt-4 feature__text'>Huong Thom Tu Dat Viet</p>
+              <p className='feature__text'>Website build using ReactJS</p>
             </Col>
 
             {featureData.map((item, index) => (
-              <Col lg="4" md="4" key={index} className="mt-5">
-                <div className="feature__item text-center px-5 py-3">
+              <Col lg='4' md='4' key={index} className='mt-5'>
+                <div className='feature__item text-center px-5 py-3'>
                   <img
                     src={item.imgUrl}
-                    alt="feature-img"
-                    className="w-25 mb-3"
+                    alt='feature-img'
+                    className='w-25 mb-3'
                   />
-                  <h5 className="fw-bold mb-3">{item.title}</h5>
+                  <h5 className='fw-bold mb-3'>{item.title}</h5>
                   <p>{item.desc}</p>
                 </div>
               </Col>
@@ -151,52 +152,117 @@ const Home = () => {
         </Container>
       </section>
 
-      <section className="section__food__category">
+      <section className='section__food__category'>
         <Container>
           <Row>
-            <Col lg="12" className="text-center">
+            <Col lg='12' className='text-center'>
               <h2>Popular Foods</h2>
             </Col>
 
-            <Col lg="12">
-              <div className="food__category d-flex align-items-center justify-content-center gap-4">
+            <Col lg='12'>
+              <div className='food__category d-flex align-items-center justify-content-center gap-4'>
                 <button
-                  className={`all__btn ${category === 'ALL' ? 'foodBtnActive' : ''}`}
-                  onClick={() => setCategory("ALL")}
+                  className={`all__btn ${
+                    category === 'ALL' ? 'foodBtnActive' : ''
+                  }`}
+                  onClick={() => setCategory('ALL')}
                 >
                   All
                 </button>
                 <button
-                  className={`d-flex align-items-center gap-2 ${category === 'BURGER' ? 'foodBtnActive' : ''}`}
-                  onClick={() => setCategory("BURGER")}
+                  className={`d-flex align-items-center gap-2 ${
+                    category === 'BURGER' ? 'foodBtnActive' : ''
+                  }`}
+                  onClick={() => setCategory('BURGER')}
                 >
-                  <img src={foodCategoryImg01} alt="" />
+                  <img src={foodCategoryImg01} alt='' />
                   Burger
                 </button>
 
                 <button
-                  className={`d-flex align-items-center gap-2 ${category === 'PIZZA' ? 'foodBtnActive' : ''}`}
-                  onClick={() => setCategory("PIZZA")}
+                  className={`d-flex align-items-center gap-2 ${
+                    category === 'PIZZA' ? 'foodBtnActive' : ''
+                  }`}
+                  onClick={() => setCategory('PIZZA')}
                 >
-                  <img src={foodCategoryImg02} alt="" />
+                  <img src={foodCategoryImg02} alt='' />
                   Pizza
                 </button>
 
                 <button
-                  className={`d-flex align-items-center gap-2 ${category === 'BREAD' ? 'foodBtnActive' : ''}`}
-                  onClick={() => setCategory("BREAD")}
+                  className={`d-flex align-items-center gap-2 ${
+                    category === 'BREAD' ? 'foodBtnActive' : ''
+                  }`}
+                  onClick={() => setCategory('BREAD')}
                 >
-                  <img src={foodCategoryImg03} alt="" />
+                  <img src={foodCategoryImg03} alt='' />
                   Bread
                 </button>
               </div>
             </Col>
 
             {allProducts.map((item) => (
-              <Col lg="3" md="4" key={item.id} className="mt-5">
+              <Col lg='3' md='4' key={item.id} className='mt-5'>
                 <ProductCard item={item} />
               </Col>
             ))}
+          </Row>
+        </Container>
+      </section>
+
+      <section className='section__why__choose'>
+        <Container>
+          <Row>
+            <Col lg='6' md='6'>
+              <img src={whyImg} alt='why-TV-food' className='w-100'/>
+            </Col>
+
+            <Col lg='6' md='6'>
+              <div className='why__TV__food'>
+                <h2 className='TV__food__title mb-4'>
+                  Why <span>TV Food?</span>
+                </h2>
+                <p className='TV__food__desc'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum, iste.</p>
+                <ListGroup className='mt-5 list__choose'>
+                  <ListGroupItem className='border-0 ps-0'>
+                    <p className='choose__title d-flex align-items-center gap-2'>
+                      <i class='ri-checkbox-circle-line'></i>
+                      Fresh and tasty foods
+                    </p>
+                    <p className='choose__desc'>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Quisquam, sapiente.
+                    </p>
+                  </ListGroupItem>
+
+                  <ListGroupItem className='border-0 ps-0'>
+                    <p className='choose__title d-flex align-items-center gap-2'>
+                      <i class='ri-checkbox-circle-line'></i>
+                      Quanlity support
+                    </p>
+                    <p className='choose__desc'>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Laudantium, repudiandae.
+                    </p>
+                  </ListGroupItem>
+
+                  <ListGroupItem className='border-0 ps-0'>
+                    <p className='choose__title d-flex align-items-center gap-2'>
+                      <i class='ri-checkbox-circle-line'>
+                      </i>
+                      Order from any location
+                    </p>
+                    <p className='choose__desc'>
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                      Numquam sequi nostrum earum totam ipsa inventore,
+                      repellendus sed corporis repudiandae reiciendis repellat
+                      eligendi quam! Fugit libero soluta et cupiditate
+                      praesentium quod.
+                    </p>
+                  </ListGroupItem>
+                </ListGroup>
+              </div>
+            </Col>
           </Row>
         </Container>
       </section>

18f279a7500ab1709ba14cfb7787a4d07e4c37ee
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2024-02-04 14:22:34 +0700

    paginate

--- src/pages/AllFood.jsx
+++ src/pages/AllFood.jsx
@@ -5,12 +5,30 @@ import Helmet from "../components/Helmet/Helmet";
 import CommonSection from "../components/UI/common-section/CommonSection";
 import products from "../assets/fake-data/products";
 import ProductCard from "../components/UI/product-card/ProductCard";
+import ReactPaginate from "react-paginate";
 
 import "../styles/all-foods.scss";
 
 const AllFood = () => {
   const [searchTerm, setSearchTerm] = useState("");
-  const [productData, setProductData] = useState(products);
+  const searchedProduct = products.filter((item) => {
+    if (searchTerm.value === "") return item;
+    if (item.title.toLowerCase().includes(searchTerm.toLowerCase()))
+      return item;
+  });
+
+  const [pageNumber, setPageNumber] = useState(0);
+  const productPerPage = 11;
+  const visitedPage = pageNumber * productPerPage;
+  const displayPage = searchedProduct.slice(
+    visitedPage,
+    visitedPage + productPerPage
+  );
+
+  const pageCount = Math.ceil(searchedProduct.length / productPerPage);
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
 
   return (
     <Helmet title="All foods">
@@ -45,19 +63,21 @@ const AllFood = () => {
               </div>
             </Col>
 
-            {productData
-              .filter(item => {
-                if (searchTerm.value === '') return item;
-                if (
-                  item.title
-                    .toLowerCase()
-                    .includes(searchTerm.toLowerCase())) return item;
-              })
-              .map((item) => (
-                <Col lg="3" md="4" sm="6" xs="6" key={item.id} className="mb-5">
-                  <ProductCard item={item} />
-                </Col>
-              ))}
+            {displayPage.map((item) => (
+              <Col lg="3" md="4" sm="6" xs="6" key={item.id} className="mb-5">
+                <ProductCard item={item} />
+              </Col>
+            ))}
+
+            <div>
+              <ReactPaginate
+                pageCount={pageCount}
+                onPageChange={changePage}
+                previousLabel="Prev"
+                nextLabel="Next"
+                containerClassName="paginationBtns"
+              />
+            </div>
           </Row>
         </Container>
       </section>

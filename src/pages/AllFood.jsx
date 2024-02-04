54775e242ab7ec47da36cee1b5d177f22b8b4645
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2024-02-04 13:09:30 +0700

    Foods search

--- src/pages/AllFood.jsx
+++ src/pages/AllFood.jsx
@@ -1,11 +1,68 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col } from "reactstrap";
+
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/common-section/CommonSection";
+import products from "../assets/fake-data/products";
+import ProductCard from "../components/UI/product-card/ProductCard";
+
+import "../styles/all-foods.scss";
 
 const AllFood = () => {
-  return (
-    <div>
-      AllFood
-    </div>
-  )
-}
+  const [searchTerm, setSearchTerm] = useState("");
+  const [productData, setProductData] = useState(products);
 
-export default AllFood
+  return (
+    <Helmet title="All foods">
+      <CommonSection title="All Foods" />
+
+      <section className="section__allFood">
+        <Container>
+          <Row>
+            <Col lg="6" md="6" sm="6">
+              <div className="search__widget d-flex align-items-center justify-content-between w-50">
+                <input
+                  type="text"
+                  placeholder="Search"
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <span>
+                  <i class="ri-search-line"></i>
+                </span>
+              </div>
+            </Col>
+
+            <Col lg="6" md="6" sm="6" className="mb-5">
+              <div className="sorting__widget text-end">
+                <select className="w-50">
+                  <option>Default</option>
+                  <option value="ascending">Alphabetically ,A-Z</option>
+                  <option value="descending">Alphabetically, Z-A</option>
+                  <option value="high-price">High Price</option>
+                  <option value="low-price">Low Price</option>
+                </select>
+              </div>
+            </Col>
+
+            {productData
+              .filter(item => {
+                if (searchTerm.value === '') return item;
+                if (
+                  item.title
+                    .toLowerCase()
+                    .includes(searchTerm.toLowerCase())) return item;
+              })
+              .map((item) => (
+                <Col lg="3" md="4" sm="6" xs="6" key={item.id} className="mb-5">
+                  <ProductCard item={item} />
+                </Col>
+              ))}
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
+
+export default AllFood;

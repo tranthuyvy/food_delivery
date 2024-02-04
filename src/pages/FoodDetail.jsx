3df14f90badba84175f8a705a8e459a00583c2dd
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2024-02-04 17:05:05 +0700

    continue productDetatil

--- src/pages/FoodDetail.jsx
+++ src/pages/FoodDetail.jsx
@@ -7,13 +7,14 @@ import CommonSection from '../components/UI/common-section/CommonSection'
 import products from '../assets/fake-data/products'
 
 import productImg from '../assets/images/product_01.1.jpg'
+import '../styles/product-detail.scss'
 
 const FoodDetail = () => {
   return (
     <Helmet title='ProductDetails'>
       <CommonSection title='Product 01'/>
 
-      <section>
+      <section className='section__productDetail'>
         <Container>
           <Row>
             <Col lg='2' md='2'>
@@ -41,13 +42,25 @@ const FoodDetail = () => {
 
             <Col lg='6' md='6'>
               <div className="single__product__content">
-                <h2 className='product__title'>Burger with cheese</h2>
-                <span className='product__price'>$24</span>
-                <p>Category: <span>Burger</span></p>
+                <h2 className='product__title mb-3'>Burger with cheese</h2>
+                <p className='product__price'>Price: <span>$24</span></p>
+                <p className='category mb-5'>Category: <span>Burger</span></p>
 
                 <button className='addToCart__btn'>Add to cart</button>
               </div>
             </Col>
+
+            <Col lg='12'>
+              <div className="tabs d-flex align-items-center gap-3 py-2">
+                <h6>Description</h6>
+                <h6>Review</h6>
+              </div>
+
+              <div className="tab__content">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis exercitationem quas accusantium nostrum eaque, fugit ea porro veritatis expedita, minima dicta, sapiente iste ad? Libero totam dolore similique nulla nam atque ad sunt doloremque. Itaque ut maxime, ipsa optio, consequuntur consectetur, pariatur eaque reiciendis odit odio quos et consequatur explicabo.</p>
+              </div>
+            </Col>
+
           </Row>
         </Container>
       </section>

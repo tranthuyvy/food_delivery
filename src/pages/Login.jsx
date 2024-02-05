c311a832da7c684d553b9d7adc9bda0446577e6c
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2024-02-05 11:12:49 +0700

    login register

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
+import React, { useRef } from "react";
+import { Link } from "react-router-dom";
+import { Container, Row, Col } from "reactstrap";
+
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/common-section/CommonSection";
+
+import "../styles/login.scss";
 
 const Login = () => {
-  return (
-    <div>
-      Login
-    </div>
-  )
-}
+  const loginNameRef = useRef();
+  const loginPasswordRef = useRef();
 
-export default Login
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Helmet title="Login">
+      <CommonSection title="Login" />
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" md="6" sm="12" className="m-auto text-center">
+              <form className="form mb-5" onSubmit={handleSubmit}>
+                <div className="form__group">
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    required
+                    ref={loginNameRef}
+                  />
+                </div>
+                <div className="form__group">
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    required
+                    ref={loginPasswordRef}
+                  />
+                </div>
+                <div>
+                  <Link to="/register">
+                    Don't have an account? Create an account
+                  </Link>
+                </div>
+                <button type="submit" className="submit__btn">
+                  Login
+                </button>
+              </form>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
+
+export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React, { useRef } from "react";
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/common-section/CommonSection";
+import { Container, Row, Col } from "reactstrap";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return (
-    <div>
-      Register
-    </div>
-  )
-}
+  const signupNameRef = useRef();
+  const signupPasswordRef = useRef();
+  const signupEmailRef = useRef();
 
-export default Register
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Helmet title="Signup">
+      <CommonSection title="Signup" />
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" md="6" sm="12" className="m-auto text-center">
+              <form className="form mb-5" onSubmit={handleSubmit}>
+                <div className="form__group">
+                  <input
+                    type="text"
+                    placeholder="Full name"
+                    required
+                    ref={signupNameRef}
+                  />
+                </div>
+                <div className="form__group">
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    required
+                    ref={signupEmailRef}
+                  />
+                </div>
+                <div className="form__group">
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    required
+                    ref={signupPasswordRef}
+                  />
+                </div>
+                <div>
+                  <Link to="/login">Already have an account? Login</Link>
+                </div>
+                <button type="submit" className="submit__btn">
+                  Sign Up
+                </button>
+              </form>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
+
+export default Register;
